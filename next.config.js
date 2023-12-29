ab0d6c9eea013e7d6f774254f063ddbfa4a699be
ClearLuvMoki <2893096286@qq.com>
2023-12-29 22:21:53 +0800

    clean

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    i18n: {
-        locales: ['en-US', 'zh-CN'],
-        defaultLocale: 'en-US',
-    },
-}
-
-module.exports = nextConfig
