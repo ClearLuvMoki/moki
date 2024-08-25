44889adef7c787e49f333e86af777678771e5ca0
moki <2893096286@qq.com>
2024-08-25 15:43:08 +0800

    add: assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-const {PHASE_DEVELOPMENT_SERVER } = require("next/constants")
-const isDev = phase === PHASE_DEVELOPMENT_SERVER
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: isDev ? undefined : 'http://www.moonquakes.online',
+    assetPrefix: 'http://www.moonquakes.online',
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
 
