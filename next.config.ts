5de85824b10e76bac25b05206d9182dc9c9c4fff
ClearLuvMoki <2893096286@qq.com>
2025-09-28 10:46:04 +0800

    fix: cache

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,19 @@ const nextConfig: NextConfig = {
                 headers: [
                     {
                         key: 'Cache-Control',
-                        value: 'no-cache, no-store, must-revalidate',
+                        value: 'no-cache, no-store, must-revalidate, max-age=0',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
+                    },
+                    {
+                        key: 'CF-Cache-Status',
+                        value: 'BYPASS',
                     },
                 ],
             },
