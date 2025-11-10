656f0b91ae5b2896b63dd26f6bbff8e3e428720e
ClearLuvMoki <2893096286@qq.com>
2025-11-10 14:21:46 +0800

    fix: moment card

--- src/app/moment/[...year]/page.tsx
+++ src/app/moment/[...year]/page.tsx
@@ -1,18 +1,18 @@
-import {GridMasonry} from "@/components/grid-masonry";
-import {MomentCard} from "@/components/moment-card";
+// import {GridMasonry} from "@/components/grid-masonry";
+// import {MomentCard} from "@/components/moment-card";
 
 const YearPage = () => {
     return <div>
-        <GridMasonry<string>
-            items={["https://avatars.githubusercontent.com/u/97165289", "https://picsum.photos/seed/NWbJM2B/640/480"]}
-            render={(item) => {
-                return <MomentCard
-                    key={item}
-                    src={item}
-                    width={300}
-                />
-            }}
-        />
+        {/*<GridMasonry<string>*/}
+        {/*    items={["https://avatars.githubusercontent.com/u/97165289", "https://picsum.photos/seed/NWbJM2B/640/480"]}*/}
+        {/*    render={(item) => {*/}
+        {/*        return <MomentCard*/}
+        {/*            key={item}*/}
+        {/*            src={item}*/}
+        {/*            width={300}*/}
+        {/*        />*/}
+        {/*    }}*/}
+        {/*/>*/}
     </div>
 }
 
